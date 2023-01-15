2d47f34500c126ebd2a96678ec962ab1d62e0beb
Trilochan DOnkena <trilochan.donkena@sensiml.com>
2023-01-14 16:04:04 -0800

    fixe class clashs

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({productionPrefix:'co'})
 
 export default () => {
   return (
+    <StylesProvider generateClassName={generateClassName}>
     <BrowserRouter>
       <div>
         <Header />        
         <MarketingApp />
       </div>
     </BrowserRouter>
+    </StylesProvider>
   );
 };
